5cb077e5050531414e6f23b70b210b48570862c4
mikesun258 <mikesun258@126.com>
2026-06-22 10:41:03 +0800

    Create RuleItem.java

---
+++ app/src/main/java/com/metaworm/skipad/RuleItem.java
@@ -0,0 +1,10 @@
+package com.metaworm.skipad;
+
+public class RuleItem {
+    public String packageName;
+    public String text;
+    public String viewId;
+    public boolean useDelay;
+    public int delay;
+    public boolean needSwipeUp;
+}
